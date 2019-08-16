afc731652675320265486a2e8baf4d0a29e6b42c
XinyaoTian <leontian1024@gmail.com>
2019-08-16 11:53:44 +0800

    Password functions complete.

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,68 @@
 from app import app
-from flask import render_template, request, redirect, url_for
-from config import Config
+from flask import render_template, url_for
+from app.forms import ResetPasswordRequestForm, ResetPasswordForm
 from func_pack import get_api_info
-from flask_login import current_user, login_user, login_required, logout_user
-from app.models import User
+from config import Config
 import requests
 
 
-id_type_checkboxs = [
-    {"id": "PF-checkbox", "name": "Platform"}
-    # ,{'id': 'IT-checkbox', 'name': 'Industry'}
-    ,
-    {"id": "AC-checkbox", "name": "Academia"},
-]
-
-id_type2_checkboxs = [
-    {"id": "DM-checkbox", "name": "Data Mining"},
-    {"id": "CV-checkbox", "name": "Computer Vision"},
-    {"id": "NLP-checkbox", "name": "Natural Language Processing"},
-    {"id": "RL-checkbox", "name": "Reinforcement Learning/Robotics"},
-    {"id": "SP-checkbox", "name": "Speech/Signal Proccessing"},
-]
+# ------------ Auth routing -------------- #
+# reset password request template
+@app.route('/reset-password-request', methods=['GET'])
+def reset_password_request():
+    form = ResetPasswordRequestForm()
+    return render_template('auth/reset_password_request.html', form=form)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    addr = Config.ADDRESS_COMP
-    info_list = get_api_info(requests.get(addr))
-    return render_template('index.html', 
-        id_type_checkboxs=id_type_checkboxs,
-    id_type2_checkboxs=id_type2_checkboxs,
-    competitions = info_list)
+# reset password request Post
+@app.route('/reset-password-request', methods=['POST'])
+def reset_password_request_recevie_form():
+    form = ResetPasswordRequestForm()
+    if form.validate_on_submit():
+        account_email = form.email.data
+        dest_url = 'http://' + Config.MAIL_SENDING_SERVICE_URL + '/api/reset-password/email-sending-by-account-email'
+        result = requests.post(dest_url, data={'account_email': account_email})
+        account_to_reset = get_api_info(result)
+        return render_template('auth/inform_reset_email.html', account_email=account_email)
 
-# @app.route("/hostby.html")
-# def hostby(id_type_checkboxs=id_type_checkboxs, hosts=hosts):
-#     return render_template(
-#         "hostby.html", id_type_checkboxs=id_type_checkboxs, hosts=hosts
-#     )
+
+# receiving password token
+@app.route('/reset-password/<string:token>', methods=['GET'])
+def reset_password_receiving_token(token):
+    form = ResetPasswordForm()
+    dest_url = 'http://' + Config.MAIL_SENDING_SERVICE_URL + '/api/reset-password/token-receiving/' +\
+               str(token)
+    result = requests.get(dest_url)
+    if result.status_code == 200:
+        account_to_reset = get_api_info(result)[0]
+        return render_template('auth/reset_password.html', form=form, account_email=account_to_reset['account_email'])
+
+
+# reset password
+@app.route('/reset-password/<string:token>', methods=['POST'])
+def reset_password(token):
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        dest_url = 'http://' + Config.MAIL_SENDING_SERVICE_URL + '/api/reset-password/token-receiving/' +\
+                   str(token)
+        new_password = str(form.re_new_password.data)
+        result = requests.get(dest_url)
+        account_to_reset = get_api_info(result)[0]
+        account_to_reset['password'] = new_password
+        update_account_url = 'http://' + Config.ACCOUNT_SERVICE_URL + '/api/account/account-updating'
+        requests.put(update_account_url, data=account_to_reset)
+        return render_template('auth/inform_reset_success.html', form=form, account_email=account_to_reset['account_email'])
+
+
+# register email sending inform
+@app.route('/register-email-sending', methods=['GET'])
+def register_email_inform():
+    return render_template('auth/inform_register_email.html')
+
+
+# register confirmation
+@app.route('/register-confirmation', methods=['GET'])
+def register_confirmation():
+    return render_template('auth/register_confirmation.html')
+
+
--- config.py
+++ config.py
@@ -4,6 +4,13 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '2c445f2022'
 
-    ADDRESS_COMP = 'http://174.137.53.253:30500/api/competition/all-competitions'
+    # for account info handling api
+    ACCOUNT_SERVICE_IP = '174.137.53.253'
+    ACCOUNT_SERVICE_PORT = '30501'
+    ACCOUNT_SERVICE_URL = ACCOUNT_SERVICE_IP + ':' + ACCOUNT_SERVICE_PORT
 
+    # for mail sending support api
+    MAIL_SENDING_SERVICE_IP = '127.0.0.1'
+    MAIL_SENDING_SERVICE_PORT = '4999'
+    MAIL_SENDING_SERVICE_URL = MAIL_SENDING_SERVICE_IP + ':' + MAIL_SENDING_SERVICE_PORT
 
