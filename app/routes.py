3ee4619f64ef8dc25ab58cdaf687596a64d95026
XinyaoTian <leontian1024@gmail.com>
2019-08-23 16:29:15 +0800

    basic test

--- app/routes.py
+++ app/routes.py
@@ -64,24 +64,6 @@ def register_confirmation(token):
         return render_template('email/register_confirmation.html')
 
 
-# register request page
-@app.route('/register-email-request/<string:account_email>', methods=['GET'])
-def register_email_request(account_email):
-    form = RegisterRequestForm()
-    return render_template('email/register_request.html', form=form, account_email=account_email)
-
-
-# sending register verification email
-@app.route('/register-email-request/<string:account_email>', methods=['POST'])
-def register_email_sending(account_email):
-    form = RegisterRequestForm()
-    if form.validate_on_submit():
-        dest_url = 'http://' + Config.MAIL_SENDING_SERVICE_URL + '/api/registration/email-sending-by-account-email'
-        result = requests.post(dest_url, data={'account_email': account_email})
-        if result.status_code == 200:
-            return render_template('email/inform_register_email.html')
-
-
 # -------------- Login View --------------- #
 # Login View
 @app.route('/login', methods=['GET'])
@@ -102,7 +84,7 @@ def login_verify():
         if result.status_code == 200:
             account = get_api_info(result)[0]
             if account['account_id'] != -1:
-                return render_template('individual/home_page.html', account_email=account['account_email'])
+                return redirect(url_for('home_view', account_email=account_email))
             else:
                 return render_template('login/login.html', form=form)
 
@@ -127,10 +109,34 @@ def register_account():
         register_url = 'http://' + Config.ACCOUNT_SERVICE_URL + '/api/account/account-creating'
         result = requests.post(register_url, data=account_info)
         if result.status_code == 200:
-            return redirect(url_for('register_email_request', account_email=form.email.data))
+            return redirect(url_for('home_view', account_email=form.email.data))
         else:
             return redirect(url_for('register_view'))
 
 
+# ---------------- Home Page ---------------- #
+# home page
+@app.route('/home/<string:account_email>', methods=['GET'])
+def home_view(account_email):
+    form = RegisterRequestForm()
+    account_info_url = 'http://' + Config.ACCOUNT_SERVICE_URL + '/api/account/account-email/' +\
+        str(account_email)
+    result = requests.get(account_info_url)
+    if result.status_code == 200:
+        account_info = get_api_info(result)[0]
+        return render_template('individual/home_page.html', form=form, account=account_info)
+    else:
+        return redirect('login_view')
+
+
+# sending registration email
+@app.route('/home/<string:account_email>', methods=['POST'])
+def home_post(account_email):
+    form_register_request = RegisterRequestForm()
+    if form_register_request.validate_on_submit():
+        dest_url = 'http://' + Config.MAIL_SENDING_SERVICE_URL + '/api/registration/email-sending-by-account-email'
+        result = requests.post(dest_url, data={'account_email': account_email})
+        if result.status_code == 200:
+            return render_template('email/inform_register_email.html')
 
 
